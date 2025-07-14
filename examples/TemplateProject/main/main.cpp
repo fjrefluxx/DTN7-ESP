3820cd535527eb8fea475ce79751cf594ad51493
Tim <tim.oehlenschlaeger@stud.tu-darmstadt.de>
2025-07-14 15:23:49 +0200

    fix include in template project

--- examples/TemplateProject/main/main.cpp
+++ examples/TemplateProject/main/main.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "dtn7idf.hpp"
+#include "dtn7-esp.hpp"
 
 extern "C" void app_main(void) {
     // Write your code here, for example begin with: Endpoint* localEndpoint = DTN7::setup("dtn://node0");
